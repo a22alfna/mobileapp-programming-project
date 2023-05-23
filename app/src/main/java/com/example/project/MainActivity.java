b12b4f06a8bf49923347d527268dcf0b55620561
a22alfna <a22alfna@student.his.se>
2023-05-23 13:26:55 +0200

    Jag lade till alla olika variabler och kopplingar mellan de olika classerna som behövs för att få appen att fungera som den ska i main activity. Jag gjorde även lite ändringar i layouten activity_main för att få de olika viewsen att se ut på rätt sätt och ligga på rätt ställe i layouten.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,18 +1,47 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=a22alfna";
+    private RecyclerView RecyclerView;
+    private ArrayList<Cities> Cities;
+    private Adapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        RecyclerView = findViewById(R.id.MyRecyclerView);
+        Cities = new ArrayList<Cities>();
+        adapter = new Adapter(Cities);
+        RecyclerView.setAdapter(adapter);
+        RecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        new JsonTask(this).execute(JSON_URL);
     }
 
+    @Override
+    public void onPostExecute(String json) {
+        Log.d("snus", "onPostExecute: " +json);
+        Gson gson = new Gson();
+        Type type = new TypeToken<ArrayList<Cities>>() {}.getType();
+        ArrayList<Cities> data = gson.fromJson(json, type);
+        Cities.addAll(data);
+        adapter.notifyDataSetChanged();
+    }
 }
