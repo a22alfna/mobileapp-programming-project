653fcf06253e66c94786d2e5387ef2367897a514
a22alfna <a22alfna@student.his.se>
2023-05-24 14:36:54 +0200

    Jag lade till den data i min adapter och min viewholder som krävdes för att både koppla dom till recycler viewn på rätt sätt och så att de olika värdena som ska displayas i recycler viewn fungerar på rätt sätt och tar emot rätt data.

--- app/src/main/java/com/example/project/Adapter.java
+++ app/src/main/java/com/example/project/Adapter.java
@@ -1,4 +1,38 @@
 package com.example.project;
 
-public class Adapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<ViewHolder> {
+
+    private List<Cities> cities;
+    public Adapter(List<Cities>cities) {
+        this.cities = cities;
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_cities, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.cityID.setText(cities.get(position).getid());
+        holder.cityName.setText(cities.get(position).getname());
+        holder.cityLocation.setText(cities.get(position).getlocation());
+        holder.citySize.setText(String.valueOf(cities.get(position).getsize()));
+        holder.cityCost.setText(String.valueOf(cities.get(position).getcost()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return cities.size();
+    }
 }
--- app/src/main/java/com/example/project/ViewHolder.java
+++ app/src/main/java/com/example/project/ViewHolder.java
@@ -1,4 +1,24 @@
 package com.example.project;
+import android.view.View;
+import android.widget.TextView;
 
-public class ViewHolder {
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+    public TextView cityID;
+    public TextView cityName;
+    public TextView cityLocation;
+    public TextView citySize;
+    public TextView cityCost;
+
+    public ViewHolder(@NonNull View itemView) {
+        super(itemView);
+        cityID = itemView.findViewById(R.id.citiesID);
+        cityName = itemView.findViewById(R.id.citiesname);
+        cityLocation = itemView.findViewById(R.id.citieslocation);
+        citySize = itemView.findViewById(R.id.citiessize);
+        cityCost = itemView.findViewById(R.id.citiescost);
+    }
+
 }
